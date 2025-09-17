aec1361e2c0e753c72a959c597641587a1707152
bflatastic <bombayapirat@gmail.com>
2025-09-17 22:30:26 +0700

    background gradient alpha

--- frontend/apps/editor3d/app/src/pages/PageVideo/BackgroundGallery.tsx
+++ frontend/apps/editor3d/app/src/pages/PageVideo/BackgroundGallery.tsx
@@ -3,7 +3,7 @@ import "./Background.css";
 export default function GalleryBackground() {
   return (
     <>
-      <div className="pointer-events-none fixed inset-0 z-[1] overflow-hidden bg-[radial-gradient(50%_50%_at_50%_50%,_transparent_49%,_rgb(var(--st-controls-rgb)_/_0.60)_100%)]" />
+      <div className="pointer-events-none fixed inset-0 z-[1] overflow-hidden bg-[radial-gradient(50%_50%_at_50%_50%,_transparent_49%,_rgb(var(--st-controls-rgb)_/_var(--st-gallery-vignette-alpha))_100%)]" />
       <div className="fixed inset-0 z-0 overflow-hidden">
         <div
           className={`video-background-image h-full w-full opacity-30 transition-opacity duration-1000`}
