363fee25104340d448402d75430a06b4b2f115d1
bflatastic <bombayapirat@gmail.com>
2026-03-12 01:00:09 +0700

    genhub init

---
+++ frontend/apps/genhub/src/app/app.tsx
@@ -0,0 +1,14 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+
+export function App() {
+  return (
+    <div>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <p className="text-2xl font-bold">GenHub</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
