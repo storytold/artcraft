962a02e65364dad888a5682f6815b4cae3ad63d9
BFlat <bombayapirat@gmail.com>
2026-03-12 15:46:27 +0700

    Revert "genhub to use nextjs instead of vite for seo (#1134)"
    This reverts commit fc81854b993feeaf5f1dd018e64cf42b7e699aab.

--- frontend/apps/genhub/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <p className="text-2xl font-bold">GenHub</p>
-    </div>
-  );
-}
--- frontend/apps/genhub/src/components/ui/menubar.tsx
+++ frontend/apps/genhub/src/components/ui/menubar.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import { Menubar as MenubarPrimitive } from "radix-ui"
 
