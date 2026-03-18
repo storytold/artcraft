964d1049af953bfeaf9d2a75485f245e4cb0ffe0
Brandon Thomas <echelon@gmail.com>
2026-03-17 22:03:19 -0400

    new ffmpeg crate

---
+++ crates/lib/ffmpeg_utils/src/lib.rs
@@ -0,0 +1,19 @@
+//! ffmpeg_utils
+//!
+//! Common tools and commands for ffmpeg and ffprobe.
+//!
+
+// Never allow these
+#![forbid(private_bounds)]
+#![forbid(private_interfaces)]
+#![forbid(unused_must_use)] // NB: It's unsafe to not close/check some things
+
+// Okay to toggle
+#![forbid(unreachable_patterns)]
+#![forbid(unused_imports)]
+#![forbid(unused_mut)]
+#![forbid(unused_variables)]
+
+// Always allow
+#![allow(dead_code)]
+#![allow(non_snake_case)]
