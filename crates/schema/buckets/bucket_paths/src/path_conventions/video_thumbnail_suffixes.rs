e16c86c0eaba9b1756bf8fdb779d1a505fa051e5
Brandon Thomas <echelon@gmail.com>
2026-03-17 21:36:31 -0400

    comment

--- crates/schema/buckets/bucket_paths/src/path_conventions/video_thumbnail_suffixes.rs
+++ crates/schema/buckets/bucket_paths/src/path_conventions/video_thumbnail_suffixes.rs
@@ -1,7 +1,11 @@
 //! Shared config for thumbnails.
 
+/// NB: DO NOT CHANGE WITHOUT MIGRATION - THIS IS SHARED BETWEEN SERVICES
 /// Video media files have conventional gif thumbnails with this suffix.
+/// This is how files are physically named in the public bucket.
 pub const VIDEO_ANIMATED_GIF_THUMBNAIL_SUFFIX: &str = "-thumb.gif";
 
+/// NB: DO NOT CHANGE WITHOUT MIGRATION - THIS IS SHARED BETWEEN SERVICES
 /// Video media files have conventional static jpg thumbnails with this suffix.
+/// This is how files are physically named in the public bucket.
 pub const VIDEO_STATIC_JPG_THUMBNAIL_SUFFIX: &str = "-thumb.jpg";
