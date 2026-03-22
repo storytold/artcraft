d8cde91c95af4f747bfc96ab8e015b0836312976
Brandon Thomas <echelon@gmail.com>
2026-03-21 23:15:39 -0400

    delete enums crate

--- crates/schema/database/sqlite_tasks/src/error.rs
+++ crates/schema/database/sqlite_tasks/src/error.rs
@@ -1,4 +1,3 @@
-use enums::error::enum_error::EnumError;
 use enums_shared::error::enums_error::EnumsError;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
@@ -6,13 +5,7 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug)]
 pub enum SqliteTasksError {
   SqlxError(sqlx::Error),
-  EnumError(EnumError),
   EnumsError(EnumsError),
-  //TaskNotFound,
-  //TaskAlreadyExists,
-  //InvalidTaskStatus,
-  //InvalidTaskType,
-  //InvalidGenerationProvider,
 }
 
 impl Error for SqliteTasksError {}
@@ -21,7 +14,6 @@ impl Display for SqliteTasksError {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
     match self {
       SqliteTasksError::SqlxError(err) => write!(f, "SQLx error: {:?}", err),
-      SqliteTasksError::EnumError(err) => write!(f, "Error parsing enum: {:?}", err),
       SqliteTasksError::EnumsError(err) => write!(f, "Error parsing enum: {:?}", err),
     }
   }
@@ -30,12 +22,6 @@ impl Display for SqliteTasksError {
 impl From<sqlx::Error> for SqliteTasksError {
   fn from(err: sqlx::Error) -> Self {
     SqliteTasksError::SqlxError(err)
-  }
-}
-
-impl From<EnumError> for SqliteTasksError {
-  fn from(err: EnumError) -> Self {
-    SqliteTasksError::EnumError(err)
   }
 }
 
