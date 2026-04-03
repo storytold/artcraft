a36f674aa72df5305d271e078378bd21f04257aa
Brandon Thomas <echelon@gmail.com>
2026-04-03 11:05:14 -0400

    Bump ArtCraft 0.20 -> 0.21

--- crates/desktop/artcraft/src/version.rs
+++ crates/desktop/artcraft/src/version.rs
@@ -1,4 +1,4 @@
 
 /// The version of the ArtCraft desktop app reported in the UI and via Analytics.
 /// This should match the CI builds.
-pub const ARTCRAFT_VERSION: &str = "0.20.0";
+pub const ARTCRAFT_VERSION: &str = "0.21.0";
