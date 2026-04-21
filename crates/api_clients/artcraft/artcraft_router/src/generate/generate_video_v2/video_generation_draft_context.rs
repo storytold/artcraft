6c0f95b1b31e167572a33665bbe58c7519ccef68
Brandon Thomas <echelon@gmail.com>
2026-04-21 16:01:10 -0400

    additional context

--- crates/api_clients/artcraft/artcraft_router/src/generate/generate_video_v2/video_generation_draft_context.rs
+++ crates/api_clients/artcraft/artcraft_router/src/generate/generate_video_v2/video_generation_draft_context.rs
@@ -1,18 +1,22 @@
-use std::collections::HashMap;
-use tokens::tokens::media_files::MediaFileToken;
 use crate::client::router_client::RouterClient;
 use crate::client::router_seedance2pro_client::RouterSeedance2ProClient;
-use crate::errors::artcraft_router_error;
 use crate::errors::artcraft_router_error::ArtcraftRouterError;
 use crate::errors::client_error::ClientError;
+use std::collections::HashMap;
+use tokens::tokens::characters::CharacterToken;
+use tokens::tokens::media_files::MediaFileToken;
 
 #[derive(Clone)]
 pub struct VideoGenerationDraftContext<'a> {
   pub client: &'a RouterClient,
-  
+
   /// Optional context: a map of Media File Tokens to their ArtCraft URLs
   /// Only needed if we have to fetch these assets and upload them to another provider.
   pub media_file_to_artcraft_url_map: Option<&'a HashMap<MediaFileToken, String>>,
+
+  /// Optional context: a map of Character Tokens to their respective Kinovi IDs
+  /// Only necessary if using Kinovi characters
+  pub character_token_to_kinovi_id_map: Option<&'a HashMap<CharacterToken, String>>,
 }
 
 impl <'a> VideoGenerationDraftContext<'a> {
@@ -20,9 +24,14 @@ impl <'a> VideoGenerationDraftContext<'a> {
     self.client.get_seedance2pro_client_ref()
         .map_err(|err| ArtcraftRouterError::Client(err))
   }
-  
+
   pub fn get_media_file_to_artcraft_url_map(&self) -> Result<&HashMap<MediaFileToken, String>, ArtcraftRouterError> {
     self.media_file_to_artcraft_url_map
         .ok_or_else(|| ArtcraftRouterError::Client(ClientError::MediaFileToUrlMapNotProvided))
-  } 
+  }
+
+  pub fn get_character_token_to_kinovi_map(&self) -> Result<&HashMap<CharacterToken, String>, ArtcraftRouterError> {
+    self.character_token_to_kinovi_id_map
+        .ok_or_else(|| ArtcraftRouterError::Client(ClientError::CharacterTokenToKinoviCharacterIdNotProvided))
+  }
 }
