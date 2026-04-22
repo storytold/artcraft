7fbe531d024a80097f90ccf8a1207cf8f67ad6dd
Brandon Thomas <echelon@gmail.com>
2026-04-21 20:15:13 -0400

    forgot file

---
+++ crates/testing/test_data/src/web/image_media_tokens.rs
@@ -0,0 +1,5 @@
+//! Production media file tokens corresponding to the image URLs in `image_urls.rs`.
+
+pub const JUNO_AT_LAKE_PRODUCTION_MEDIA_TOKEN: &str = "m_m1bz02z1kkzanxy6rb4vk1kvq9de9g";
+pub const FOREST_BACKDROP_PRODUCTION_MEDIA_TOKEN: &str = "m_f5yy0fdmkwrf2hxaq4yw4sd95tvvrf";
+pub const WHITE_HOUSE_SUNSET_PRODUCTION_MEDIA_TOKEN: &str = "m_xmzdeqhjpxs5418kvepcrj5a3vkfb1";
