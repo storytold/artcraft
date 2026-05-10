024c834d8d14ec28d3eaebe7a1e99b09fc4653ba
Brandon Thomas <echelon@gmail.com>
2026-05-10 09:11:14 -0400

    mj

--- crates/desktop/artcraft/src/core/providers/provider_credential_key.rs
+++ crates/desktop/artcraft/src/core/providers/provider_credential_key.rs
@@ -4,16 +4,17 @@ use crate::core::providers::provider_credential_type::ProviderCredentialType;
 #[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum ProviderCredentialKey {
-  
+
   // ========== API KEYS==========
-  
+
   FalApiKey,
   ReplicateApiKey,
-  
+
   // ========== WEB LOGINS ==========
-  
-  HiggsfieldWebLogin,
+
   GrokWebLogin,
+  HiggsfieldWebLogin,
+  MidjourneyLogin,
   RunwayWebLogin,
 }
 
@@ -25,18 +26,22 @@ impl ProviderCredentialKey {
       Self::FalApiKey => ProviderCredentialType::ApiKey,
       Self::ReplicateApiKey => ProviderCredentialType::ApiKey,
       // Web logins
-      Self::HiggsfieldWebLogin => ProviderCredentialType::WebLogin,
       Self::GrokWebLogin => ProviderCredentialType::WebLogin,
+      Self::HiggsfieldWebLogin => ProviderCredentialType::WebLogin,
+      Self::MidjourneyLogin => ProviderCredentialType::WebLogin,
       Self::RunwayWebLogin => ProviderCredentialType::WebLogin,
     }
   }
-  
+
   pub fn get_filename(&self) -> &'static str {
     match self {
+      // Api keys
       ProviderCredentialKey::FalApiKey => "fal.api_key.txt",
       ProviderCredentialKey::ReplicateApiKey => "replicate.api_key.txt",
-      ProviderCredentialKey::HiggsfieldWebLogin => "higgsfield.web_login.toml",
+      // Web logins
       ProviderCredentialKey::GrokWebLogin => "grok.web_login.toml",
+      ProviderCredentialKey::HiggsfieldWebLogin => "higgsfield.web_login.toml",
+      ProviderCredentialKey::MidjourneyLogin => "midjourney.web_login.toml",
       ProviderCredentialKey::RunwayWebLogin => "runway.web_login.toml",
     }
   }
