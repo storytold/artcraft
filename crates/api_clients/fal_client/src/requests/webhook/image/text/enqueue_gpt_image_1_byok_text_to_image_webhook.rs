5aca0db36a5a37620b45f7dfac449621e23e76db
Brandon Thomas <echelon@gmail.com>
2026-05-14 18:21:56 -0400

    rename

--- crates/api_clients/fal_client/src/requests/webhook/image/text/enqueue_gpt_image_1_byok_text_to_image_webhook.rs
+++ crates/api_clients/fal_client/src/requests/webhook/image/text/enqueue_gpt_image_1_byok_text_to_image_webhook.rs
@@ -8,7 +8,7 @@ use crate::creds::open_ai_api_key::OpenAiApiKey;
 use crate::error::classify_fal_error::classify_fal_error;
 use crate::error::fal_error_plus::FalErrorPlus;
 use crate::requests::traits::fal_request_cost_calculator_trait::{FalRequestCostCalculator, UsdCents};
-use crate::requests::http::image::text::http_gpt_image_1_text_to_image::{gpt_image_1_text_to_image, GptImage1TextToImageInput};
+use crate::requests::http::image::text::http_gpt_image_1_byok_text_to_image::{gpt_image_1_text_to_image, GptImage1TextToImageInput};
 use crate::requests::core_api::webhook_response::WebhookResponse;
 use reqwest::IntoUrl;
 
