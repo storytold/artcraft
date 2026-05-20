13f937ae007a5a1cf029d0bf619390a2774dee36
Brandon Thomas <echelon@gmail.com>
2026-05-20 08:57:27 -0400

    cleanup

--- crates/api_clients/artcraft/artcraft_router/src/generate/generate_video_v2/providers/artcraft/mod.rs
+++ crates/api_clients/artcraft/artcraft_router/src/generate/generate_video_v2/providers/artcraft/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod build_common;
 pub mod happy_horse_1p0;
 pub mod preview_model;
 pub mod preview_model_fast;
+pub(crate) mod request_common;
 pub(crate) mod resolve;
 pub mod seedance_2p0;
 pub mod seedance_2p0_fast;
--- crates/api_clients/artcraft/artcraft_router/src/generate/generate_video_v2/providers/artcraft/preview_model_fast/request.rs
+++ crates/api_clients/artcraft/artcraft_router/src/generate/generate_video_v2/providers/artcraft/preview_model_fast/request.rs
@@ -1,32 +1,17 @@
 use artcraft_api_defs::omni_gen::cost_and_generate_requests::omni_gen_video_cost_and_generate_request::OmniGenVideoCostAndGenerateRequest;
-use artcraft_client::endpoints::omni_gen::generate::video::omni_gen_video::omni_gen_video_generate;
 
 use crate::client::router_artcraft_client::RouterArtcraftClient;
 use crate::errors::artcraft_router_error::ArtcraftRouterError;
-use crate::errors::provider_error::ProviderError;
-use crate::generate::generate_video::generate_video_response::{
-  ArtcraftVideoResponsePayload, GenerateVideoResponse,
-};
+use crate::generate::generate_video::generate_video_response::GenerateVideoResponse;
+use crate::generate::generate_video_v2::providers::artcraft::request_common::send_artcraft_omni_request;
 
 #[derive(Clone, Debug)]
 pub struct ArtcraftPreviewModelFastRequestState {
-  /// Final materialized request; ready to fire via the omni-gen video endpoint.
   pub request: OmniGenVideoCostAndGenerateRequest,
 }
 
 impl ArtcraftPreviewModelFastRequestState {
   pub async fn send(&self, client: &RouterArtcraftClient) -> Result<GenerateVideoResponse, ArtcraftRouterError> {
-    let response = omni_gen_video_generate(
-      &client.api_host,
-      Some(&client.credentials),
-      self.request.clone(),
-    )
-      .await
-      .map_err(|err| ArtcraftRouterError::Provider(ProviderError::Storyteller(err)))?;
-
-    Ok(GenerateVideoResponse::Artcraft(ArtcraftVideoResponsePayload {
-      inference_job_token: response.inference_job_token.clone(),
-      all_inference_job_tokens: vec![response.inference_job_token],
-    }))
+    send_artcraft_omni_request(&self.request, client).await
   }
 }
