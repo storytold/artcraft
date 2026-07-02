c65a66655acdd25033ca4afbcaff2f74fdd582a8
Brandon Thomas <echelon@gmail.com>
2026-07-02 16:38:11 -0400

    performance

--- _database/sql/migrations/2026-07-02-192433-0000_alter_debug_logs_add_log_level/up.sql
+++ _database/sql/migrations/2026-07-02-192433-0000_alter_debug_logs_add_log_level/up.sql
@@ -3,4 +3,4 @@
 -- noinspection SqlResolveForFile
 
 ALTER TABLE debug_logs
-  ADD COLUMN maybe_log_level VARCHAR(16) DEFAULT NULL AFTER debug_log_type;
+  ADD COLUMN maybe_log_level VARCHAR(16) DEFAULT NULL;
